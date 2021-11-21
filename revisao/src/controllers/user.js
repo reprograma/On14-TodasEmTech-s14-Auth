8e5b4831da0580d29656667fbc68fac3500b9754
Jéss <oskojess.com.br>
2021-11-21 15:51:57 -0300

    tarefa: adicionar autorização

--- revisao/src/controllers/user.js
+++ revisao/src/controllers/user.js
@@ -1,5 +1,10 @@
-const User = require('../models/User');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
+const User = require('../models/User');
+const { hashPassword } = require('../helpers/auth')
+
+//sem rota privada
 const getAll = async(req, res) => {
     try {
         const users = await User.find()
@@ -11,25 +16,83 @@ const getAll = async(req, res) => {
     }
 }
 
+//com rota privada
+const getUsers = async(req, res) => {
+    //passar o id por parametro
+    const id = req.params.id
+
+    //buscar pelo id e trazer todas as infos. menos a senha
+    const user = await User.findById(id, '-password')
+
+    if (!user) {
+        return res.status(422).send({ message: "Não encontramos nenhum cadastro com a informação passada." })
+    }
+
+    res.status(200).json({
+        user
+    })
+}
 const register = async(req, res) => {
     const { name, email, password } = req.body
-    try {
 
+    try {
         const newUser = new User({
             name,
             email,
             password
         })
 
+        //tratar o campo password para ser criptografado
+        const passwordHashed = await hashPassword(newUser.password, res)
 
+        newUser.password = passwordHashed
+
+        const saveUser = await newUser.save()
+        res.status(201).json({
+            messagem: "Pessoa cadastrada com sucesso",
+            saveUser
+        })
 
     } catch (error) {
-
+        res.status(500).json({
+            message: error.message
+        })
     }
+}
 
+const login = async(req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email: email })
+
+        if (!user) {
+            return res.status(422).send({ message: "Email não encontrado!" })
+        }
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if (!checkPassword) {
+            return res.status(422).send({ message: "Senha incorreta!" })
+        }
+
+        const secret = process.env.SECRET
+        const token = jwt.sign({ id: user._id }, secret)
+
+        res.status(200).json({
+            message: "Token deu bom!",
+            token
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
 }
 
 
 module.exports = {
-    getAll
+    getAll,
+    getUsers,
+    register,
+    login
 }
--- revisao/src/routes/user.js
+++ revisao/src/routes/user.js
@@ -2,10 +2,32 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/user')
+const { checkAuth } = require('../middlewares/auth')
 
 //@route GET /api/users/
-//@desc Listar todos os usuários
+//@desc Listar todos os cadastros
 //@access Public
 router.get('/', userController.getAll)
+
+//@route GET /api/users/:id
+//@desc Listar todos os cadastros
+//@access Private
+router.get('/:id', checkAuth, userController.getAll)
+
+//@route POST /api/users/register
+//@desc Criar conta
+//@access Public
+router.post('/register', userController.register)
+
+//@route POST /api/users/login
+//@desc Acessar conta
+//@access Public
+router.post('/login', userController.login)
+
+//@route GET /api/users/
+//@desc Acessar conta
+//@access Public
+router.post('/login', userController.login)
+
 
 module.exports = router
